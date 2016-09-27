ab4ac6c61ab35bf118cb063e026bf9d795c8581f
Koprvhdix <454898060@qq.com>
2016-09-27 22:24:32 +0800

    finish poem_preprocess

--- poem_preprocess.py
+++ poem_preprocess.py
@@ -40,21 +40,31 @@ if __name__ == '__main__':
             file_open = codecs.open(final_path, 'w', 'utf-8')
             file_open.write(final_content)
 
+    ignore_poem_file = codecs.open('ignore_poem_file.txt', 'w', 'utf-8')
+    for item in ignore_poem:
+        ignore_poem_file.write((str(item) + '\n').decode('ascii').encode('utf-8'))
+    ignore_poem_file.close()
+
+    # 用来存储分类结果
+    answer_file = codecs.open('answer_file.txt', 'w', 'utf-8')
+
     # 从赏析中获取抒情还是叙事的特征，方法：含【抒】字，或者含两个【情】字的为抒情诗，其他的为叙事诗，记录在最后一行。
     poem_dir = 'poem'
     file_list = os.listdir(poem_dir)
     answer_statistic = [0, 0]  # 抒情诗的数量和叙事诗的数量
+
     for item in file_list:
         if item in ignore_poem:
             print item
             continue
 
-        file_open = codecs.open(poem_dir + '/' + item, 'rw', 'utf-8')
-        file_read = file_open.readlines()
         final_path = 'poem/' + item
         answer = 0
         key_vector = [0, 0]  # 第一个为【抒】字的数量，第二个为【情】字的数量
 
+        file_open = codecs.open(poem_dir + '/' + item, 'rw', 'utf-8')
+        file_read = file_open.readlines()
+        file_open.close()
         for key in file_read[3]:
             if key == u'抒':
                 if answer == 0:
@@ -65,10 +75,11 @@ if __name__ == '__main__':
                 if answer == 0 and key_vector[1] == 2:
                     answer = 1
 
+        answer_file.write((item + ' ' + str(answer) + '\n').decode('ascii').encode('utf-8'))
+
         key_vector.append(answer)
-        print item, key_vector
         answer_statistic[answer] += 1
-        # 写入文件
+        # 写入文件 为了校验方便
         final_str = u''
         file_read[3] += '\n'
         if answer == 0:
@@ -81,4 +92,6 @@ if __name__ == '__main__':
             final_str += lines
         file_open = codecs.open(final_path, 'w', 'utf-8')
         file_open.write(final_str)
+        file_open.close()
     print answer_statistic
+    answer_file.close()
